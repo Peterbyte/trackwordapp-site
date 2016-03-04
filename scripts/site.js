e79e3829edd1418118583c8fad18462946f6fefe
Peter Bouquet <peterbyte@users.noreply.github.com>
2016-03-04 23:20:44 +0000

    Use OwlCarousel for slide images

---
+++ scripts/site.js
@@ -0,0 +1,14 @@
+$(function(){
+	$('.owl-carousel').owlCarousel({
+		items: 1,
+		singleItem: true,
+		lazyLoad: true,
+		lazyEffect: false,
+		navigation: false,
+		pagination: false,
+		mouseDrag: false,
+		touchDrag: false,
+		autoPlay: 4000,
+		transitionStyle: "fade"
+	});
+});
